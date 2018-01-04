a357f80bdc8e905655ff6fb28ed41ab0561a6eac
Abraham Lin <al684@cornell.edu>
2018-01-04 20:32:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ function theBeatlesPlay(musician, instrument) {
   var musicianArray = [];
   for (i = 0; i<musician.length; i++) {
     musicianArray[i] = `${musician[i]} plays ${instrument[i]}.`;
-    return musicianArray;
   }
+  return musicianArray;
 }
